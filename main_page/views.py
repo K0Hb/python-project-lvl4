aa3dfb643b6bf74d820a9782c1a71d918bfd3193
Вячеслав <goplit2010.konovalov@yandex.ru>
2021-12-08 17:43:36 +0300

    add title

--- main_page/views.py
+++ main_page/views.py
@@ -6,7 +6,7 @@ from django.views.generic.edit import CreateView
 from django.utils.translation import gettext as _
 
 class HomePageView(TemplateView):
-    extra_context = {'title': _('Главная страница')}
+    # extra_context = {'title': _('Главная страница')}
     template_name = 'main_page/home_page.html'
 
 class UsersListView(ListView):
--- task_manager/settings.py
+++ task_manager/settings.py
@@ -121,6 +121,8 @@ LANGUAGES = (
     ('ru', _('Russian')),
 )
 
+LOCALE_PATHS = [os.path.join(BASE_DIR, 'locale'), ]
+
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
