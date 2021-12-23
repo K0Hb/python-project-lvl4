2959b6406b9d1c2cfe951ca8897abf19d9cf8a83
Вячеслав <goplit2010.konovalov@yandex.ru>
2021-12-23 10:51:12 +0300

    add placeholders

--- main_page/forms.py
+++ main_page/forms.py
@@ -15,17 +15,18 @@ class RegisterUserForm(UserCreationForm):
         self.fields['password2'].widget = forms.PasswordInput(
             attrs={'class': 'form-control',
                    'placeholder': 'Подтверждение пароля'})
+        self.fields['username'].widget = forms.TextInput(
+            attrs={'class': 'form-control',
+                   'placeholder': 'Имя пользователя'})
+        self.fields['first_name'].widget = forms.TextInput(
+            attrs={'class': 'form-control',
+                   'placeholder': 'Имя'})
+        self.fields['last_name'].widget = forms.TextInput(
+            attrs={'class': 'form-control',
+                   'placeholder': 'Фамилия'})
 
     class Meta(UserCreationForm.Meta):
         fields = ['username', 'first_name', 'last_name']
-        widgets = {
-            'username': forms.TextInput(
-                attrs={'placeholder': 'Имя пользователя'}),
-            'first_name': forms.TextInput(
-                attrs={'placeholder': 'Имя'}),
-            'last_name': forms.TextInput(
-                attrs={'placeholder': 'Фамилия'}),
-        }
 
 
 class AuthUserForm(AuthenticationForm, forms.ModelForm):
