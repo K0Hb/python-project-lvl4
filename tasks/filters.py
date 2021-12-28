c7f4af5a0737693c6dc01523b07c249cb20f4409
Вячеслав <goplit2010.konovalov@yandex.ru>
2021-12-28 15:16:47 +0300

    edit filter

--- tasks/filters.py
+++ tasks/filters.py
@@ -7,9 +7,15 @@ from django.forms.widgets import CheckboxInput
 
 class TaskFilter(django_filters.FilterSet):
     own_tasks = django_filters.BooleanFilter(
+        label='Только свои задачи',
         method='filter_own_tasks',
-        # field_name='creator',
+        field_name='creator',
         widget=CheckboxInput
+    )
+
+    tags = django_filters.ModelChoiceFilter(
+        queryset=Tags.objects.all(),
+        label='Метки'
     )
 
     def filter_own_tasks(self, queryset, name, value):
@@ -17,10 +23,12 @@ class TaskFilter(django_filters.FilterSet):
             return queryset.filter(creator=self.request.user).order_by('pk')
         return queryset
 
-    tags = django_filters.ModelChoiceFilter(
-        queryset=Tags.objects.all(),
-    )
-
     class Meta:
         model = Task
         fields = ['executor', 'status', 'tags', 'creator']
+        filter_overrides = {
+            django_filters.BooleanFilter: {
+                'extra': lambda f: {'widget': CheckboxInput},
+                'filter_class': django_filters.BooleanFilter,
+            },
+        }
