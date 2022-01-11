1bbf026df79b26d1e6d12a0553b3a9b5cd08a795
Вячеслав <goplit2010.konovalov@yandex.ru>
2022-01-11 12:40:43 +0300

    add env

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'webserver').split(' ')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='*').split(' ')
 
 INSTALLED_APPS = [
     'django.contrib.contenttypes',
